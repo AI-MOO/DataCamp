ca7e02adc33b465d646745c09046dc6e701b0bf6
AI-MOO <eemoh.osman@gmail.com>
2022-05-09 07:45:56 +0300

    module: a function counts words in text

---
+++ Data_Scientist_with_Python/16-Intermediate_Importing_Data_in_Python/modules/words_text.py
@@ -0,0 +1,10 @@
+import re
+
+def word_in_text(word, text):
+    word = word.lower()
+    text = text.lower()
+    match = re.search(word, text)
+
+    if match:
+        return True
+    return False
